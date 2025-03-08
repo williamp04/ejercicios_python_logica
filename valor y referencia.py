7da4f93951cbbafefe2dfe490a6448c5e9044e11
williamp04 <williampintogonzalez04@gmail.com>
2025-03-08 14:11:37 -0500

    feat: :construction: new step

--- valor y referencia.py
+++ valor y referencia.py
@@ -36,10 +36,46 @@ print(my_int_c)
 
 
 
-def my_list_func(my_list: list):
-    my_list.append(30)
-    print(my_list)
+"""def my_list_func(my_list: list):
+    print(my_list_d)
 
 my_list_c = [10, 20 ]
 my_list_func(my_list_c)
-print(my_list_c)
+print(my_list_c)"""
+
+
+"""
+extra
+"""
+
+# por valor
+
+
+def value(value_a: int, value_b: int) -> tuple:
+    temp = value_a
+    value_a = value_b
+    value_b = temp
+    return value_a, value_b  
+
+
+my_int_d = 10
+my_int_e = 20
+my_int_f, my_int_g = value(my_int_d, my_int_e)
+
+print(f"{my_int_d}, {my_int_e}") 
+print(f"{my_int_f}, {my_int_g}")  
+
+
+#por referencia
+
+def ref(value_a: list, value_b: list) -> tuple:
+    temp = value_a
+    value_a = value_b
+    value_b = temp
+    return value_a, value_b
+
+my_list_e = [10, 20]
+my_int_f = [30, 40]
+my_int_g, my_int_h = ref(my_list_e, my_int_f)
+print(f"{my_list_e}, {my_int_f}")
+print(f"{my_int_g}, {my_int_h}")
